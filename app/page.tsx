90666581b801af9bb63aa51728a49fff69c50f57
rortan134 <samaritanojr006@gmail.com>
2024-05-20 11:35:09 +0200

    up

--- app/page.tsx
+++ app/page.tsx
@@ -15,59 +15,55 @@ import * as React from "react";
 export const dynamic = "error";
 
 const color = [200, 0, 255] as const;
-const divergence_threshold = 50;
-const depth = 300;
-const zoom_amount = 0.8;
+const divergencia = 50;
+const profundidad = 300;
+const multiplicadorZoom = 0.8;
 
 export default function HomePage() {
   const canvasRef = React.useRef<HTMLCanvasElement | null>(null);
   const locationRef = React.useRef([-0.5, 0] as [number, number]);
   const scaleRef = React.useRef(2);
 
-  // Plot the mandelbrot function on the canvas
-  function draw_mandelbrot(
-    location: [number, number],
-    scale: number,
-    divergence_threshold: number,
-    depth: number,
+  function dibuixar_mandelbrot(
+    posicion: [number, number],
+    escala: number,
+    valor_divergencia: number,
+    profundidad: number,
     canvas: HTMLCanvasElement,
     ctx: CanvasRenderingContext2D,
   ) {
-    const escape_value = divergence_threshold * divergence_threshold;
+    const escape_value = valor_divergencia * valor_divergencia;
     const width = canvas.width;
     const height = canvas.height;
     const image_data = ctx.getImageData(0, 0, width, height);
     const rgb = new Uint8ClampedArray(image_data.data.buffer);
-    const start = performance.now();
 
     for (let i = 0; i < width; i++) {
       // Get the initial c value for our function f(z) = z^2 + c
-      const real_component = map(
+      const componente_real = map(
         i,
         0,
         width,
-        location[0] - scale,
-        location[0] + scale,
+        posicion[0] - escala,
+        posicion[0] + escala,
       );
       for (let j = 0; j < height; j++) {
-        const imaginary_component = map(
+        const componente_imaginario = map(
           j,
           0,
           height,
-          location[1] - scale,
-          location[1] + scale,
+          posicion[1] - escala,
+          posicion[1] + escala,
         );
-        let count = 0;
+        let contador = 0;
 
-        // Initial z values (z is complex)
         let real = 0;
         let imaginary = 0;
 
-        // Iterate f(z) = z^2 + c for a given number of steps
-        while (count < depth) {
+        while (contador < profundidad) {
           const temp_real =
-            real * real - imaginary * imaginary + real_component;
-          imaginary = 2 * real * imaginary + imaginary_component;
+            real * real - imaginary * imaginary + componente_real;
+          imaginary = 2 * real * imaginary + componente_imaginario;
           real = temp_real;
 
           // Exit the loop early if we determine that the function has diverged
@@ -75,31 +71,29 @@ export default function HomePage() {
             break;
           }
 
-          count++;
+          contador++;
         }
 
         const rgb_index = (i + j * width) * 4;
 
-        // Fill with black if the point is within the Mandelbrot set
-        if (count == depth) {
+        if (contador == profundidad) {
+          // Punt no fa part de la fractal, el pintem de negre
           rgb[rgb_index] = 0;
           rgb[rgb_index + 1] = 0;
           rgb[rgb_index + 2] = 0;
         } else {
-          // Otherwise, color it progressively more intense as it took more iterations to diverge in our loop
-          const intensity = Math.sqrt(map(count, 0, depth, 0, 1));
-          rgb[rgb_index] = intensity * color[0];
-          rgb[rgb_index + 1] = intensity * color[1];
-          rgb[rgb_index + 2] = intensity * color[2];
+          // Punt fa part de la fractal, el pintem de color
+          const intensidad = Math.sqrt(map(contador, 0, profundidad, 0, 1));
+          rgb[rgb_index] = intensidad * color[0];
+          rgb[rgb_index + 1] = intensidad * color[1];
+          rgb[rgb_index + 2] = intensidad * color[2];
         }
       }
     }
-    const end = performance.now();
-    console.log(`Rendered in ${end - start}ms`);
+
     ctx.putImageData(image_data, 0, 0);
   }
 
-  // Map an input in a given range to another range
   function map(
     input: number,
     input_min: number,
@@ -107,22 +101,19 @@ export default function HomePage() {
     output_min: number,
     output_max: number,
   ) {
-    const normalized_input = (input - input_min) / (input_max - input_min);
-    const mapped_input =
-      output_min + normalized_input * (output_max - output_min);
-    return mapped_input;
+    const input_normalizado = (input - input_min) / (input_max - input_min);
+    const input_mapped =
+      output_min + input_normalizado * (output_max - output_min);
+    return input_mapped;
   }
 
-  // Zooming and keeping clicked pixel in place on screen for smoothness
   function handle_zoom(event: React.MouseEvent, canvas: HTMLCanvasElement) {
     const ctx = canvas.getContext("2d", { willReadFrequently: true });
     if (!ctx) return;
 
-    // Get where the user clicked in terms of the canvas width/height coordinate system
     const click_x = event.clientX;
     const click_y = event.clientY;
 
-    // Map the user's click to the range of real/imaginary numbers represented on the canvas
     const mapped_x = map(
       click_x,
       0,
@@ -138,44 +129,40 @@ export default function HomePage() {
       locationRef.current[1] + scaleRef.current,
     );
 
-    // Move the origin
     locationRef.current[0] =
-      mapped_x - (mapped_x - locationRef.current[0]) * zoom_amount;
+      mapped_x - (mapped_x - locationRef.current[0]) * multiplicadorZoom;
     locationRef.current[1] =
-      mapped_y - (mapped_y - locationRef.current[1]) * zoom_amount;
-    // Zoom and draw
-    scaleRef.current *= zoom_amount;
+      mapped_y - (mapped_y - locationRef.current[1]) * multiplicadorZoom;
+    scaleRef.current *= multiplicadorZoom;
 
-    draw_mandelbrot(
+    dibuixar_mandelbrot(
       locationRef.current,
       scaleRef.current,
-      divergence_threshold,
-      depth,
+      divergencia,
+      profundidad,
       canvas,
       ctx,
     );
   }
 
-  // Go back to original fractal
-  function reset_fractal(canvas: HTMLCanvasElement) {
+  function reiniciar_fractal(canvas: HTMLCanvasElement) {
     const ctx = canvas.getContext("2d", {
       willReadFrequently: true,
     });
     if (!ctx) return;
     locationRef.current = [-0.5, 0];
     scaleRef.current = 1.5;
-    draw_mandelbrot(
+    dibuixar_mandelbrot(
       locationRef.current,
       scaleRef.current,
-      divergence_threshold,
-      depth,
+      divergencia,
+      profundidad,
       canvas,
       ctx,
     );
   }
 
-  // Clear the canvas
-  function reset_canvas(
+  function reiniciar_canvas(
     canvas: HTMLCanvasElement,
     ctx: CanvasRenderingContext2D,
   ) {
@@ -183,8 +170,10 @@ export default function HomePage() {
     ctx.fillRect(0, 0, canvas.width, canvas.height);
   }
 
-  // Make everything fit on screen
-  function fit(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
+  function recalcularVentana(
+    canvas: HTMLCanvasElement,
+    ctx: CanvasRenderingContext2D,
+  ) {
     if (window.innerWidth * 0.65 > window.innerHeight * 0.8) {
       canvas.height = Math.floor(window.innerHeight * 0.8 * 0.75);
       canvas.width = canvas.height;
@@ -192,10 +181,10 @@ export default function HomePage() {
       canvas.width = Math.floor(window.innerWidth * 0.65 * 0.75);
       canvas.height = canvas.width;
     }
-    reset_canvas(canvas, ctx);
+    reiniciar_canvas(canvas, ctx);
   }
 
-  React.useLayoutEffect(() => {
+  React.useLayoutEffect(function effect() {
     const canvas = canvasRef.current;
     if (!canvas) return;
     const ctx = canvas.getContext("2d", {
@@ -203,21 +192,21 @@ export default function HomePage() {
     });
     if (!ctx) return;
 
-    const res = () => fit(canvas, ctx);
+    const res = () => recalcularVentana(canvas, ctx);
     // Ensure everything fits on screen when resized
     window.addEventListener("resize", res);
-    draw_mandelbrot(
+    dibuixar_mandelbrot(
       locationRef.current,
       scaleRef.current,
-      divergence_threshold,
-      depth,
+      divergencia,
+      profundidad,
       canvas,
       ctx,
     );
 
     return () => {
       window.removeEventListener("resize", res);
-      reset_canvas(canvas, ctx);
+      reiniciar_canvas(canvas, ctx);
     };
   }, []);
 
@@ -244,11 +233,11 @@ export default function HomePage() {
               iterades en matemàtiques
             </Heading>
             <Text size="2" color="gray" style={{ maxWidth: "20rem" }}>
-              Fes clic per a augmentar la escala del fractal
+              Fes clic per a augmentar la escala de la fractal
             </Text>
             <Button
               style={{ width: "fit-content" }}
-              onClick={() => reset_fractal(canvasRef.current!)}
+              onClick={() => reiniciar_fractal(canvasRef.current!)}
             >
               Reiniciar
             </Button>
@@ -263,13 +252,13 @@ export default function HomePage() {
             const ctx = node.getContext("2d", { willReadFrequently: true });
             if (!ctx) return;
             // Resize everything upon launch and plot initial fractal
-            fit(node, ctx);
+            recalcularVentana(node, ctx);
             // Draw Mandelbrot when the page opens
-            draw_mandelbrot(
+            dibuixar_mandelbrot(
               locationRef.current,
               scaleRef.current,
-              divergence_threshold,
-              depth,
+              divergencia,
+              profundidad,
               node,
               ctx,
             );
