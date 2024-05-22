0bf8f4a65bb7676b6a94414ec86543fc163cb02c
rortan134 <samaritanojr006@gmail.com>
2024-05-22 22:53:37 +0200

    styles

--- app/page.tsx
+++ app/page.tsx
@@ -217,10 +217,10 @@ export default function HomePage() {
       role="main"
       className="relative isolate h-screen w-full"
     >
-      <Section>
+      <Section style={{touchAction: "none"}}>
         <Container>
-          <Flex direction="column" gap="2">
-            <Flex gap="2">
+          <Flex direction="column" gap="2" style={{maxWidth: "20rem"}}>
+            <Flex gap="2" style={{ pointerEvents: "none"}}>
               <Badge size="2" style={{ width: "fit-content" }}>
                 Gilberto Samaritano Junior
               </Badge>
@@ -228,11 +228,11 @@ export default function HomePage() {
                 f(z) = z² + c
               </Badge>
             </Flex>
-            <Heading size="5" weight="bold" style={{ maxWidth: "20rem" }}>
+            <Heading size="5" weight="bold" style={{ maxWidth: "20rem", pointerEvents: "none" }}>
               Demostració. Fractal de Mandelbrot: Ús artistic de les funcions
               iterades en matemàtiques
             </Heading>
-            <Text size="2" color="gray" style={{ maxWidth: "20rem" }}>
+            <Text size="2" color="gray" style={{ maxWidth: "20rem", pointerEvents: "none" }}>
               Fes clic per a augmentar la escala de la fractal
             </Text>
             <Button
